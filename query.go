1d0631e14249e2516ff2f00ba243a1edd96849a8
Marco Brotas <marco@mediamonks.com>
2019-08-02 14:16:14 +0200

    Fix typo.

--- query.go
+++ query.go
@@ -1,6 +1,6 @@
 package query
 
-// Query is the interface that must be implemented by all queries
+// Query is the interface that must be implemented by any type to be considered a query.
 type Query interface {
 	ID() []byte
 }
